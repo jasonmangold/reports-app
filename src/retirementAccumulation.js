61c47799a5c356d14d993cd7fa592e32389b9ed1
jasonmangold <jmangold@advisys.com>
2025-05-06 11:05:42 -0700

    Update retirementAccumulation.js

--- src/retirementAccumulation.js
+++ src/retirementAccumulation.js
@@ -335,7 +335,7 @@ function calculateRetirementIncome(clientData, getAge) {
       // Calculate Social Security
       let socialSecurity = 0;
       if (currentC1Age >= c1RetirementAge && currentC1Age <= c1MaxAge) {
-        socialSecurity += (parseFloat(clientData.client1.incomeSources.socialSecurity) || 0) * 12azionali: true,
+        socialSecurity += (parseFloat(clientData.client1.incomeSources.socialSecurity) || 0) * 12: true,
         stack: true,
         bar: true,
         data: [
